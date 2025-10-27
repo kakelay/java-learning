9c1681ae768824cd37e55dd793be9a3d50fb4ded
Kak Elay <Elay.Kak@amkcambodia.com>
2025-10-27 13:33:46 +0700

    115. Step 03 - First Spring MVC Controller, @ResponseBody, @Controller

---
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,15 @@
+package com.in28minutes.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+    //http://localhost:8080/say-hello
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHello( ) {
+        return "Hello!!! what are you doing";
+    }
+}
