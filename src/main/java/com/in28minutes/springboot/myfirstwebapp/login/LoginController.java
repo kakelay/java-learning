1f475ca0edf33d75d760a1b7a1ca218e5339d8f3
Kak Elay <Elay.Kak@amkcambodia.com>
2025-11-04 16:58:23 +0700

    123. Step 09 - Quick Overview - Importance of Logging with Spring Boot

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,20 +1,19 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-    ///  login => com.in28minutes.springboot.myfirstwebapp.login.LoginController.login => login.jsp
-    /// http://localhost:8080/login?name=Elay
-    /// Model
+    
     @RequestMapping("login")
-    public String gotoLoginPage(@RequestParam String cid ,  ModelMap model) {
-        model.put("cid" , cid);
-        System.out.println("cid is : " + cid );
+    public String gotoLoginPage( @RequestParam String cid, ModelMap model ) {
+        model.put( "cid", cid );
         return "login";
     }
+    
 }
