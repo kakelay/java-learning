b54bcaf1f6535b38a040f717a3582b577e44a1b3
kakelay <kakelay18052002@gmail.com>
2026-04-19 17:29:43 +0700

    update port back to 8080 and add config zipkin

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/controller/user/UserController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/controller/user/UserController.java
@@ -3,6 +3,11 @@ package com.in28minutes.springboot.myfirstwebapp.controller.user;
 import com.in28minutes.springboot.myfirstwebapp.common.BaseResponse;
 import com.in28minutes.springboot.myfirstwebapp.dto.response.UserResponse;
 import com.in28minutes.springboot.myfirstwebapp.service.UserService;
+import io.micrometer.observation.annotation.Observed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import io.micrometer.tracing.Tracer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.MediaType;
@@ -14,12 +19,16 @@ import java.util.UUID;
 @RestController
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     private final MessageSource messageSource;
     private final UserService userService;
+    private final Tracer tracer;
 
-    public UserController(MessageSource messageSource, UserService userService) {
+    public UserController(MessageSource messageSource, UserService userService, @Autowired(required = false) Tracer tracer) {
         this.messageSource = messageSource;
         this.userService = userService;
+        this.tracer = tracer;
     }
 
     private String generateReference() {
@@ -27,9 +36,16 @@ public class UserController {
     }
 
     @GetMapping(value = "/user" , produces = MediaType.APPLICATION_JSON_VALUE)
+    @Observed(name = "user.get", contextualName = "get-user")
     public ResponseEntity<BaseResponse<UserResponse>> getUser() {
 
         String ref = generateReference();
+
+        if (tracer != null && tracer.currentSpan() != null) {
+            logger.info("Processing user request with traceId: {}", tracer.currentSpan().context().traceId());
+        } else {
+            logger.info("Processing user request (no tracing available)");
+        }
 
         String msg = messageSource.getMessage(
                 "response.success.message",
