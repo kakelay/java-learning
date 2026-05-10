4c16bdfddd7f215416fe9432d6dce58baff4b09a
kakelay <kakelay18052002@gmail.com>
2026-05-10 13:12:57 +0700

    update clean code don't used UserServiceImpl for default code

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/service/impl/UserServiceImpl.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/service/impl/UserServiceImpl.java
@@ -242,18 +242,9 @@ public class UserServiceImpl implements UserService {
     }
 
     private UserResponse getDefaultUserResponse() {
-        return UserResponse.builder()
-                .cid("USR001")
-                .name("Elay Kak")
-                .phone("010600261")
-                .email("kakelay18052002@gmail.com")
-                .address("123 Monivong Blvd")
-                .city("Phnom Penh")
-                .state("Phnom Penh")
-                .country("Cambodia")
-                .zipCode("12000")
-                .profilePic("https://avatars.githubusercontent.com/u/110383694?v=4")
-                .website("https://kakelay-dev.vercel.app/")
-                .build();
+        return userRepository.findByActiveTrue().stream()
+                .findFirst()
+                .map(this::convertToUserResponse)
+                .orElseGet(this::getDefaultUserResponse);
     }
 }
